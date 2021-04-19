42524a0e7bdd210ab22f39c9609588bde58da0d7
theartofdevil <miracunlu98@gmail.com>
2021-04-19 18:33:21 +0300

    Initial Commit

---
+++ vending machine arduino/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  vending machine arduino
+//
+//  Created by Mr.Engineer on 2021-04-19.
+//  Copyright © 2021 Mr.Engineer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
